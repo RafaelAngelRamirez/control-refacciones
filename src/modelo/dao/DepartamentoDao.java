12ba380c1b115ec8234f0208e09b6c8dbbb8d2c2
Rafael Ramirez <rafa.yael@gmail.com>
2017-08-17 11:18:18 -0500

    Dao creados para las clases.

---
+++ src/modelo/dao/DepartamentoDao.java
@@ -0,0 +1,16 @@
+
+package modelo.dao;
+
+import controlador.Coordinador;
+
+/**
+ *
+ * @author Particular
+ */
+public class DepartamentoDao extends DAOGenerales{
+    
+    public DepartamentoDao(Coordinador coordinador) {
+        super(coordinador);
+    }
+    
+}
---
+++ src/modelo/dao/EmpleadoDao.java
@@ -0,0 +1,16 @@
+
+package modelo.dao;
+
+import controlador.Coordinador;
+
+/**
+ *
+ * @author Particular
+ */
+public class EmpleadoDao extends DAOGenerales{
+    
+    public EmpleadoDao(Coordinador coordinador) {
+        super(coordinador);
+    }
+    
+}
