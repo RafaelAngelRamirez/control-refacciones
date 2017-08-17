35b24f7b7fb768a67a4fcaf06a44a1ff8cea0ae5
Rafael Ramirez <rafa.yael@gmail.com>
2017-08-17 08:59:44 -0500

    EntradaLoteDao creada.

---
+++ src/modelo/dao/EntradaLoteDao.java
@@ -0,0 +1,21 @@
+
+package modelo.dao;
+
+import controlador.Coordinador;
+
+/**
+ *
+ * @author Particular
+ */
+public class EntradaLoteDao extends DAOGenerales{
+    
+    public EntradaLoteDao(Coordinador coordinador) {
+        super(coordinador);
+    }
+    
+    
+    
+    
+    
+    
+}
