6a051a546c4fdc700197c092432c55e4ef824a93
Rafael Ramirez <rafa.yael@gmail.com>
2017-08-22 17:29:52 -0500

    EmpleadoDao terminado.

--- src/modelo/dao/EmpleadoDao.java
+++ src/modelo/dao/EmpleadoDao.java
@@ -3,7 +3,10 @@ package modelo.dao;
 
 import controlador.Coordinador;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.InfoTabla.EmpleadoIT;
 import modelo.vo.EmpleadoVo;
 
@@ -32,13 +35,44 @@ public class EmpleadoDao extends DAOGenerales{
         mapa.put(2, vo.getIdDepartamento());
         
         ResultSet r = conexion.executeQuery(sql, mapa);
-        
-        
-        
-           
-                
-    
+        try {
+            r.next();
+            int i = r.getInt(1);
+            if (i>0) 
+                return true;
+            return false;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
+    
+    
+    public boolean guardar(EmpleadoVo vo){
+        String sql = "INSERT INTO " + EmpleadoIT.NOMBRE_TABLA 
+                +" VALUES (null, ?, ?)";
+        
+        HashMap<Integer, Object> mapa = new HashMap<>();
+        mapa.put(1, vo.getNombre());
+        mapa.put(2, vo.getIdDepartamento());
+        return conexion.executeUpdate(sql, mapa);
+    }
+    
+    public int consultarUltimoId(){
+        String sql = "SELECT MAX(" + it.getIdPDC().getNombre() + ") FROM "+EmpleadoIT.NOMBRE_TABLA;
+        ResultSet r = conexion.executeQuery(sql);
+        try {
+            r.next();
+            return r.getInt(1);
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;    
+    }
+    
+    
+    
     
     
     
