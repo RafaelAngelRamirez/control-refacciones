23b256547a5d1574fd6e6a272b42a88fe38cd11d
Rafael Ramirez <rafa.yael@gmail.com>
2017-08-28 12:49:06 -0500

    dao,vo, it de entrada de control.

---
+++ src/modelo/vo/EntradaDeControlVo.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.vo;
+
+import java.sql.Timestamp;
+import modelo.InfoTabla.EntradaDeControlIT;
+
+/**
+ *
+ * @author Particular
+ */
+public class EntradaDeControlVo extends VoGenerales{
+
+    
+    private int id;
+    private Object idRefaccion;
+    private Object idMaquinaEnLaQueSeUsara;
+    private Object idEmpleado;
+    private Timestamp fechaSalida;
+    private float cantidad;
+    private String observaciones;
+
+    public EntradaDeControlVo() {
+        
+        EntradaDeControlIT it = new EntradaDeControlIT();
+        relacionCampo.put(it.getIdPDC().getNombre(), ()->this.getId());
+        relacionCampo.put(it.getIdRefaccionPDC().getNombre(), ()->this.getIdRefaccion());
+        relacionCampo.put(it.getIdMaquinaEnLaQueSeUsaraPDC().getNombre(), ()->this.getIdMaquinaEnLaQueSeUsara());
+        relacionCampo.put(it.getIdEmpleadoPDC().getNombre(), ()->this.getIdEmpleado());
+        relacionCampo.put(it.getFechaSalidaPDC().getNombre(), ()->this.getFechaSalida());
+        relacionCampo.put(it.getCantidadPDC().getNombre(), ()->this.getCantidad());
+        relacionCampo.put(it.getObservacionesPDC().getNombre(), ()->this.getObservaciones());
+    
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Object getIdRefaccion() {
+        return idRefaccion;
+    }
+
+    public void setIdRefaccion(Object idRefaccion) {
+        this.idRefaccion = idRefaccion;
+    }
+
+    public Object getIdMaquinaEnLaQueSeUsara() {
+        return idMaquinaEnLaQueSeUsara;
+    }
+
+    public void setIdMaquinaEnLaQueSeUsara(Object idMaquinaEnLaQueSeUsara) {
+        this.idMaquinaEnLaQueSeUsara = idMaquinaEnLaQueSeUsara;
+    }
+
+    public Object getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Object idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Timestamp getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Timestamp fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
