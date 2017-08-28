f2bfeee1abca9489645c747757207ae8d3d29453
Rafael Ramirez <rafa.yael@gmail.com>
2017-08-28 11:13:56 -0500

    Dao creado

---
+++ src/modelo/dao/SalidaDeControlDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import controlador.Coordinador;
+import modelo.InfoTabla.SalidaDeControlIT;
+
+/**
+ *
+ * @author Particular
+ */
+public class SalidaDeControlDao extends DAOGenerales{
+    
+    SalidaDeControlIT it;
+    
+    public SalidaDeControlDao(Coordinador coordinador) {
+        super(coordinador);
+        it = new SalidaDeControlIT();
+        
+    }
+    
+    
+    
+}
