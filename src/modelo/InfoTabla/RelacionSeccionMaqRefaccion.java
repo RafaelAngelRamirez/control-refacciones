4a56d0093f649e5cb662daa8241f22ba1f11653d
Rafael Ramirez <rafa.yael@gmail.com>
2017-10-16 10:21:25 -0500

    Fin de sección. Nueva sección Seccion de maquinas.

---
+++ src/modelo/InfoTabla/RelacionSeccionMaqRefaccion.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.InfoTabla;
+
+/**
+ *
+ * @author Particular
+ */
+public class RelacionSeccionMaqRefaccion extends ITGenerales{
+    
+     /*
+    * El nombre de la tabla en la base de datos. 
+    */
+    public static final String NOMBRE_TABLA = "relacionseccionmaqrefaccion";
+    
+    private ParametrosDeCampo idRefaccionPDC = new ParametrosDeCampo();
+    private ParametrosDeCampo idSeccionMaquinaPDC = new ParametrosDeCampo();
+
+    public RelacionSeccionMaqRefaccion() {
+        idRefaccionPDC.setNombre("idRefaccion");
+        idRefaccionPDC.setLongitudDeCaracteres(11);
+        idRefaccionPDC.setTipoDeDatos("int");
+        idRefaccionPDC.setNulo(false);
+        idRefaccionPDC.setAutoIncrement(false);
+        idRefaccionPDC.setPermiteRepetido(true);
+
+        idSeccionMaquinaPDC.setNombre("idSeccionMaquina");
+        idSeccionMaquinaPDC.setLongitudDeCaracteres(11);
+        idSeccionMaquinaPDC.setTipoDeDatos("int");
+        idSeccionMaquinaPDC.setNulo(false);
+        idSeccionMaquinaPDC.setAutoIncrement(false);
+        idSeccionMaquinaPDC.setPermiteRepetido(true);
+        
+        camposPDC.add(idRefaccionPDC);
+        camposPDC.add(idSeccionMaquinaPDC);
+      
+        
+    }
+
+    public ParametrosDeCampo getIdRefaccionPDC() {
+        return idRefaccionPDC;
+    }
+
+    public void setIdRefaccionPDC(ParametrosDeCampo idRefaccionPDC) {
+        this.idRefaccionPDC = idRefaccionPDC;
+    }
+
+    public ParametrosDeCampo getIdSeccionMaquinaPDC() {
+        return idSeccionMaquinaPDC;
+    }
+
+    public void setIdSeccionMaquinaPDC(ParametrosDeCampo idSeccionMaquinaPDC) {
+        this.idSeccionMaquinaPDC = idSeccionMaquinaPDC;
+    }
+    
+    
+}
---
+++ src/modelo/InfoTabla/SeccionDeMaquinaIT.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.InfoTabla;
+
+/**
+ *
+ * @author Particular
+ */
+public class SeccionDeMaquinaIT extends ITGenerales{
+    
+     /*
+    * El nombre de la tabla en la base de datos. 
+    */
+    public static final String NOMBRE_TABLA = "secciondemaquina";
+    
+    private ParametrosDeCampo idPDC = new ParametrosDeCampo();
+    private ParametrosDeCampo nombreSeccion = new ParametrosDeCampo();
+ 
+
+    public SeccionDeMaquinaIT() {
+        idPDC.setNombre("id");
+        idPDC.setLongitudDeCaracteres(11);
+        idPDC.setTipoDeDatos("int");
+        idPDC.setNulo(false);
+        idPDC.setAutoIncrement(true);
+        idPDC.setPermiteRepetido(false);
+
+        nombreSeccion.setNombre("nombreSeccion");
+        nombreSeccion.setNombreParaMostrar("Nombre de seccion");
+        nombreSeccion.setLongitudDeCaracteres(30);
+        nombreSeccion.setTipoDeDatos("varchar");
+        nombreSeccion.setNulo(false);
+        nombreSeccion.setAutoIncrement(false);
+        nombreSeccion.setPermiteRepetido(false);
+        
+              
+        camposPDC.add(idPDC);
+        camposPDC.add(nombreSeccion);
+       
+    
+    
+    }
+    
+    
+    
+}
