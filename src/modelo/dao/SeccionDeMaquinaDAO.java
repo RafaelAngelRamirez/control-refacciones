be895d81a0916d7ea9ede5b35b56a3d2a8ce0bf7
Rafael Ramirez <rafa.yael@gmail.com>
2017-10-16 11:57:11 -0500

    Dao creados.

---
+++ src/modelo/dao/SeccionDeMaquinaDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import controlador.Coordinador;
+
+/**
+ *
+ * @author Particular
+ */
+public class SeccionDeMaquinaDAO extends DAOGenerales{
+    
+    public SeccionDeMaquinaDAO(Coordinador coordinador) {
+        super(coordinador);
+    }
+    
+}
