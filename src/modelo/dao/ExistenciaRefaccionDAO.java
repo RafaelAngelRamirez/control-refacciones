0767e582745c37efd1a99328d368e87c4ac1ff6f
Rafael Ramirez <rafa.yael@gmail.com>
2017-11-21 18:02:09 -0600

    Actualizcion a tabla de existencias echa.

--- src/modelo/dao/ExistenciaRefaccionDAO.java
+++ src/modelo/dao/ExistenciaRefaccionDAO.java
@@ -5,10 +5,39 @@
  */
 package modelo.dao;
 
+import controlador.Coordinador;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Conexion;
+import modelo.InfoTabla.ExistenciaRefaccionIT;
+
 /**
  *
  * @author Particular
  */
-public class ExistenciaRefaccionDAO {
+public class ExistenciaRefaccionDAO extends DAOGenerales{
+
+    public ExistenciaRefaccionDAO(Coordinador coordinador) {
+        super(coordinador);
+    }
+
+    public float existencia(int id) {
+        try {
+            conexion = new Conexion(coordinador);
+            String sql = "SELECT " + ExistenciaRefaccionIT.getEXISTENCIA().getNombre()
+                    + " FROM " + ExistenciaRefaccionIT.NOMBRE_TABLA
+                    + " WHERE  " + ExistenciaRefaccionIT.getID_REFACCION().getNombre() +" = ?";
+            
+            ResultSet r = conexion.executeQuery(sql, id);
+            r.next();
+            return r.getFloat(ExistenciaRefaccionIT.getEXISTENCIA().getNombre());
+        } catch (SQLException ex) {
+            Logger.getLogger(ExistenciaRefaccionDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+        return 0;
+    }
     
 }
